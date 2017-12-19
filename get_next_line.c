4849cb061b3251501ca92faf9e76b83bdfcc026c
Gaspard LEGENDRE <glegendr@e1r11p20.42.fr>
2017-12-19 21:26:18 +0100

    Correction Leack

--- get_next_line.c
+++ get_next_line.c
@@ -6,31 +6,29 @@
 /*   By: glegendr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/18 20:58:22 by glegendr          #+#    #+#             */
-/*   Updated: 2017/12/19 18:29:22 by glegendr         ###   ########.fr       */
+/*   Updated: 2017/12/19 21:24:47 by glegendr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int			ft_return_value(t_vec *vec, char ***line, int r, int i)
+int			ft_return_value(t_vec *vec, char **line, int r, int i)
 {
 	t_vec	vec2;
 	void	*pos;
 
 	if (i == 1)
 	{
-		vec2 = v_new(1);
 		vec2 = v_split(vec, r);
 		pos = v_raw(&vec2);
 		((char *)pos)[r] = 0;
-		**line = pos;
+		*line = pos;
 		return (1);
 	}
-	vec2 = v_new(1);
 	vec2 = v_split(vec, v_size(vec) - 1);
 	pos = v_raw(&vec2);
 	((char *)pos)[v_size(&vec2)] = 0;
-	**line = pos;
+	*line = pos;
 	return (1);
 }
 
@@ -43,7 +41,7 @@ int			get_next_line(const int fd, char **line)
 	void			*tmp;
 
 	if (v_raw(&vec) == NULL)
-		vec = v_new(1);
+		vec = v_new(sizeof(char));
 	while ((ret = read(fd, s, BUFF_SIZE)) > 0 || v_size(&vec) != 0)
 	{
 		if (ret == -1)
@@ -53,9 +51,9 @@ int			get_next_line(const int fd, char **line)
 		tmp = v_raw(&vec);
 		while (r < v_size(&vec))
 			if (((char *)tmp)[r++] == '\n')
-				return (ft_return_value(&vec, &line, r - 1, 1));
+				return (ft_return_value(&vec, line, r - 1, 1));
 		if (ret == 0)
-			return (ft_return_value(&vec, &line, 0, 0));
+			return (ft_return_value(&vec, line, 0, 0));
 	}
 	if (ret == 0)
 		return (0);
